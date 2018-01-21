e7a405a56f183d2f9c013719b3ea43b63531bf51
jimhigson <jim.higson@gmail.com>
2018-01-21 12:19:29 +0000

    use my eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,8 @@
-'use strict';
+// https://eslint.org/docs/user-guide/configuring
 
-module.exports = {
-  parser: 'babel-eslint',
-  extends: 'idiomatic',
-  rules: {
-    "one-var": 'off',
-    "arrow-parens": ["error", "as-needed"],
-    "semi": ["error", "never"],
-    "indent": ["error", 4],
-    "max-len": [
-      "error",
-      { "code": 100,
-        "ignoreTemplateLiterals": true,
-        "ignoreStrings": true
-      }
-    ]
-  }
-}
+const defaultsDeep = require( 'lodash.defaultsdeep' );
+
+module.exports = defaultsDeep( {
+    parser: 'babel-eslint'
+    // project-specific rules here
+}, require( 'jimhigson-my-eslint-rules' ) )
